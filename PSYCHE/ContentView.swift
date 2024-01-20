893223db44b27470195940e95dba0d69543214ac
piacobelli14 <dinolabsauthentication@outlook.com>
2024-01-20 11:06:09 -0600

    Initial PSYCHE Commit

---
+++ PSYCHE/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PSYCHE
+//
+//  Created by Peter Iacobelli on 1/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PSYCHE/PSYCHEApp.swift
@@ -0,0 +1,17 @@
+//
+//  PSYCHEApp.swift
+//  PSYCHE
+//
+//  Created by Peter Iacobelli on 1/20/24.
+//
+
+import SwiftUI
+
+@main
+struct PSYCHEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
