d678e550f32eede38318cbacaac0e971bf8410e9
piacobelli14 <dinolabsauthentication@outlook.com>
2024-01-21 17:22:44 -0600

    x

---
+++ PSYCHE/Views/PSYCHEManage/PSYCHEPatients.swift
@@ -0,0 +1,20 @@
+//
+//  PSYCHEPatients.swift
+//  PSYCHE
+//
+//  Created by Peter Iacobelli on 1/21/24.
+//
+
+import SwiftUI
+
+struct PSYCHEPatients: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PSYCHEPatients_Previews: PreviewProvider {
+    static var previews: some View {
+        PSYCHEPatients()
+    }
+}
