438531b117c871e0ba3ed85b3049c76e78e09abd
piacobelli14 <dinolabsauthentication@outlook.com>
2024-01-22 13:03:20 -0600

    update

--- PSYCHE/Views/PSYCHEManage/PSYCHEPatients.swift
+++ PSYCHE/Views/PSYCHEManage/PSYCHEPatients.swift
@@ -18,8 +18,19 @@ struct PatientInfo: Codable {
 struct PSYCHEPatients: View {
     @Binding var currentView: AppView
     @State private var patients: [PatientInfo] = []
-    @State private var selectedPatientID: String?
+    @State private var selectedPatientID: String = ""
     @State private var patientSearch: String = ""
+    @State private var isCurrent = true
+    
+    private var filteredPatients: [PatientInfo] {
+        if patientSearch.isEmpty {
+            return patients
+        } else {
+            return patients.filter { patient in
+                return patient.ptname.lowercased().contains(patientSearch.lowercased())
+            }
+        }
+    }
 
     var body: some View {
         ZStack {
@@ -97,7 +108,7 @@ struct PSYCHEPatients: View {
                         
                         HStack {
                             Spacer()
-                            List(patients, id: \.ptid) { patient in
+                            List(filteredPatients, id: \.ptid) { patient in
                                 HStack {
                                     Spacer()
                                     Image(systemName: selectedPatientID == patient.ptid ? "checkmark.square.fill" : "square")
@@ -106,7 +117,7 @@ struct PSYCHEPatients: View {
                                         .font(.system(size: geometry.size.height * 0.025))
                                         .onTapGesture {
                                             if selectedPatientID == patient.ptid {
-                                                selectedPatientID = nil
+                                                selectedPatientID = ""
                                             } else {
                                                 selectedPatientID = patient.ptid
                                             }
@@ -130,7 +141,7 @@ struct PSYCHEPatients: View {
                                             Text("Unit: \(patient.pttag)").font(.subheadline)
                                         }
                                         .frame(width: geometry.size.width * 0.25)
-                                        .font(.system(size: geometry.size.height * 0.015, weight: .semibold))
+                                        .font(.system(size: geometry.size.height * 0.012, weight: .semibold))
                                     }
                                     .frame(width: geometry.size.width * 0.6)
                                     .foregroundColor(Color.white)
@@ -165,33 +176,107 @@ struct PSYCHEPatients: View {
                             HStack {
                                 Spacer()
                                 HStack(spacing: geometry.size.width * 0.05) {
-                                    Image(systemName: "person.badge.plus")
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fill)
-                                        .frame(width: geometry.size.width * 0.02, height: geometry.size.height * 0.02)
-                                        .foregroundColor(Color(hex: 0x152234))
-                                        .shadow(color: .gray, radius: geometry.size.width * 0.01)
                                     
-                                    Image(systemName: "person.badge.minus")
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fill)
-                                        .frame(width: geometry.size.width * 0.02, height: geometry.size.height * 0.02)
-                                        .foregroundColor(Color(hex: 0x152234))
-                                        .shadow(color: .gray, radius: geometry.size.width * 0.01)
                                     
-                                    Image(systemName: "square.and.pencil")
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fill)
-                                        .frame(width: geometry.size.width * 0.02, height: geometry.size.height * 0.02)
-                                        .foregroundColor(Color(hex: 0x152234))
-                                        .shadow(color: .gray, radius: geometry.size.width * 0.01)
                                     
-                                    Image(systemName: "applewatch")
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fill)
-                                        .frame(width: geometry.size.width * 0.02, height: geometry.size.height * 0.02)
-                                        .foregroundColor(Color(hex: 0x152234))
-                                        .shadow(color: .gray, radius: geometry.size.width * 0.01)
+                                    HStack {
+                                        Spacer()
+                                        Text("Archive")
+                                            .font(.system(size: geometry.size.height * 0.01, weight: isCurrent ? .regular : .bold))
+                                            .multilineTextAlignment(.center)
+                                            .foregroundColor(isCurrent ? Color(hex: 0x152234) : Color(hex: 0x4E7FD5))
+                                            .scaleEffect(isCurrent ? 1.0 : 1.2)
+                                            
+                                        Spacer()
+
+                                        Toggle(isOn: $isCurrent) {}
+                                            .toggleStyle(SwitchToggleStyle(tint: Color(hex: 0x4E7FD5)))
+                                            .labelsHidden()
+                                            .multilineTextAlignment(.center)
+                                            .padding(.horizontal, geometry.size.width * 0.01)
+                                            .onChange(of: isCurrent) { newValue in
+                                                fetchPatientInfo()
+                                            }
+
+                                        Spacer()
+
+                                        Text("Current")
+                                            .font(.system(size: geometry.size.height * 0.01, weight: isCurrent ? .bold : .regular))
+                                            .multilineTextAlignment(.center)
+                                            .foregroundColor(isCurrent ? Color(hex: 0x4E7FD5) : Color(hex: 0x152234))
+                                            .scaleEffect(isCurrent ? 1.2 : 1.0)
+                                            
+                                        Spacer()
+                                    }
+                                    .frame(width: geometry.size.width * 0.25)
+                                    .padding(.trailing, geometry.size.width * 0.1)
+                                    
+                                    Spacer()
+                                    
+                                    Button(
+                                        action: {
+                                            //
+                                        },
+                                        label: {
+                                            Image(systemName: "person.badge.plus")
+                                                .resizable()
+                                                .aspectRatio(contentMode: .fill)
+                                                .frame(width: geometry.size.width * 0.02, height: geometry.size.height * 0.02)
+                                                .foregroundColor(Color(hex: 0x152234))
+                                                .shadow(color: .gray, radius: geometry.size.width * 0.01)
+                                        }
+                                    )
+                                    .disabled(selectedPatientID == "" ? true : false)
+                                    .opacity(selectedPatientID == "" ? 0.6 : 1)
+                                    
+                                    Button(
+                                        action: {
+                                            //
+                                        },
+                                        label: {
+                                            Image(systemName: "person.badge.minus")
+                                                .resizable()
+                                                .aspectRatio(contentMode: .fill)
+                                                .frame(width: geometry.size.width * 0.02, height: geometry.size.height * 0.02)
+                                                .foregroundColor(Color(hex: 0x152234))
+                                                .shadow(color: .gray, radius: geometry.size.width * 0.01)
+                                        }
+                                    )
+                                    .disabled(selectedPatientID == "" ? true : false)
+                                    .opacity(selectedPatientID == "" ? 0.6 : 1)
+                                    
+                                    Button(
+                                        action: {
+                                            //
+                                        },
+                                        label: {
+                                            Image(systemName: "square.and.pencil")
+                                                .resizable()
+                                                .aspectRatio(contentMode: .fill)
+                                                .frame(width: geometry.size.width * 0.02, height: geometry.size.height * 0.02)
+                                                .foregroundColor(Color(hex: 0x152234))
+                                                .shadow(color: .gray, radius: geometry.size.width * 0.01)
+                                        }
+                                    )
+                                    .disabled(selectedPatientID == "" ? true : false)
+                                    .opacity(selectedPatientID == "" ? 0.6 : 1)
+                                    
+                                    Button(
+                                        action: {
+                                            //
+                                        },
+                                        label: {
+                                            Image(systemName: "applewatch")
+                                                .resizable()
+                                                .aspectRatio(contentMode: .fill)
+                                                .frame(width: geometry.size.width * 0.02, height: geometry.size.height * 0.02)
+                                                .foregroundColor(Color(hex: 0x152234))
+                                                .shadow(color: .gray, radius: geometry.size.width * 0.01)
+                                        }
+                                    )
+                                    .disabled(selectedPatientID == "" ? true : false)
+                                    .opacity(selectedPatientID == "" ? 0.6 : 1)
+                                    
                                 }
                             }
                             .frame(width: geometry.size.width * 0.9)
@@ -212,44 +297,50 @@ struct PSYCHEPatients: View {
     }
 
     private func fetchPatientInfo() {
-            let url = URL(string: "http://10.111.26.70:8001/get-patients-current")!
-            var request = URLRequest(url: url)
-            request.httpMethod = "GET"
-            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let requestBody: [String: Any] = [
+            "patientTable": (isCurrent ? "current" : "archive"),
+        ]
 
-            URLSession.shared.dataTask(with: request) { data, response, error in
-                if let error = error {
-                    DispatchQueue.main.async {
-                        print("Error: \(error.localizedDescription)")
-                    }
-                    return
+        let url = URL(string: "http://10.111.26.70:8001/get-patients")!
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpBody = try? JSONSerialization.data(withJSONObject: requestBody)
+
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                DispatchQueue.main.async {
+                    print("Error: \(error.localizedDescription)")
                 }
+                return
+            }
 
-                guard let data = data, let response = response as? HTTPURLResponse else {
-                    DispatchQueue.main.async {
-                        print("No data or response.")
-                    }
-                    return
+            guard let data = data, let response = response as? HTTPURLResponse else {
+                DispatchQueue.main.async {
+                    print("No data or response.")
                 }
+                return
+            }
 
-                if response.statusCode == 200 {
-                    do {
-                        let decodedData = try JSONDecoder().decode([PatientInfo].self, from: data)
-                        DispatchQueue.main.async {
-                            self.patients = decodedData
-                            print("Fetched patients: \(self.patients)")
-                        }
-                    } catch {
-                        DispatchQueue.main.async {
-                            print("Error decoding JSON: \(error.localizedDescription)")
-                        }
-                    }
-                } else {
+            if response.statusCode == 200 {
+                do {
+                    let decodedData = try JSONDecoder().decode([PatientInfo].self, from: data)
                     DispatchQueue.main.async {
-                        print("HTTP Status Code: \(response.statusCode)")
+                        self.patients = decodedData
+                        print("Fetched patients: \(self.patients)")
                     }
+                } catch {
+                    DispatchQueue.main.async {
+                        print("Error decoding JSON: \(error.localizedDescription)")
+                    }
+                }
+            } else {
+                DispatchQueue.main.async {
+                    print("HTTP Status Code: \(response.statusCode)")
                 }
             }
-            .resume()
         }
+        .resume()
     }
+}
